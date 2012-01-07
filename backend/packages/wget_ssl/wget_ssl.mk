2a1461b4292ebfe3498013be7f1a1f2fe909d16b
unknown <tbrandstetter@MacPro.(none)>
2012-01-07 14:56:59 +0100

    correct wget mirror

--- backend/packages/wget_ssl/wget_ssl.mk
+++ backend/packages/wget_ssl/wget_ssl.mk
@@ -5,6 +5,7 @@
 #############################################################
 
 WGET_SSL_VERSION = 1.13.4
+WGET_SSL_SOURCE = wget-$(WGET_VERSION).tar.bz2
 WGET_SSL_SITE = $(BR2_GNU_MIRROR)/wget
 WGET_SSL_CONF_OPT += --with-ssl=gnutls --with-libgnutls-prefix=$(STAGING_DIR)
 WGET_SSL_DEPENDENCIES += gnutls
