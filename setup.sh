8d5687fb5cce45e755cc5e281782a27a83361ee9
Thomas Brandstetter <tb@digitalplayground.at>
2012-01-22 17:00:37 +0100

    redefine directory structure for ARM support

--- setup.sh
+++ setup.sh
@@ -39,11 +39,11 @@ BUILDROOT_VERSION=2011.05
 ARCH=$1
 BASE=`pwd`
 WORKDIR=$2
-BOOT_HOME=$WORKDIR/boot_$ARCH/boot/isolinux
+BOOT_HOME=$WORKDIR/$ARCH/boot/boot/isolinux
 MOUNT_CD=$WORKDIR/mount/cdrom
 MOUNT_PULSAR=$WORKDIR/mount/pulsar
-PACKAGE_DIR=$WORKDIR/corepackages
-GCC_DIR=$WORKDIR/gcc
+PACKAGE_DIR=$WORKDIR/$ARCH/corepackages
+GCC_DIR=$WORKDIR/$ARCH/gcc
 
 # Build the pulsar os image
 # Functions
@@ -52,36 +52,36 @@ prepare_pulsar ()
 {
 	echo "Prepare $ARCH build"
 	# create necessary directories
-	[ ! -f $WORKDIR/.prepared ] && mkdir -p $BOOT_HOME $MOUNT_CD $MOUNT_PULSAR $WORKDIR/images $GCC_DIR
+	[ ! -f $WORKDIR/$ARCH/build/.prepared ] && mkdir -p $BOOT_HOME $MOUNT_CD $MOUNT_PULSAR $WORKDIR/images $GCC_DIR
 	[ -d $WORKDIR/configs ] && rm -rf $WORKDIR/configs
 	[ -d $WORKDIR/pulsarroot ] && rm -rf $WORKDIR/pulsarroot
 	[ -d $WORKDIR/frontend ] && rm -rf $WORKDIR/frontend
 	[ -d $WORKDIR/startupscripts ] && rm -rf $WORKDIR/startupscripts
-	[ -d $WORKDIR/build_$ARCH/output ] && rm -rf $WORKDIR/build_$ARCH/output
+	[ -d $WORKDIR/$ARCH/build/output ] && rm -rf $WORKDIR/$ARCH/build/output
 	[ -f $BOOT_HOME/initrd.bz2 ] && rm $BOOT_HOME/initrd*
 	[ -f $BOOT_HOME/bzImage ] && rm $BOOT_HOME/bzImage
-	[ -d $WORKDIR/core ] && rm -rf $WORKDIR/core
-	[ -d $WORKDIR/sdk ] && rm -rf $WORKDIR/sdk
-	if [ -d $WORKDIR/boot_$ARCH/core ]; then
-		rm  $WORKDIR/boot_$ARCH/core/*
+	[ -d $WORKDIR/$ARCH/core ] && rm -rf $WORKDIR/$ARCH/core
+	[ -d $WORKDIR/$ARCH/sdk ] && rm -rf $WORKDIR/$ARCH/sdk
+	if [ -d $WORKDIR/$ARCH/boot/core ]; then
+		rm  $WORKDIR/$ARCH/boot/core/*
 	else
-		mkdir -p $WORKDIR/boot_$ARCH/core
+		mkdir -p $WORKDIR/$ARCH/boot/core
 	fi
-	[ ! -f $WORKDIR/boot_$ARCH/.pulsarinstall ] && touch $WORKDIR/boot_$ARCH/.pulsarinstall
+	[ ! -f $WORKDIR/$ARCH/boot/.pulsarinstall ] && touch $WORKDIR/$ARCH/boot/.pulsarinstall
 }
 
 stage_pulsar ()
 {
 	echo "Download & prepare buildroot"
-	if [ ! -d $WORKDIR/build_$ARCH ]; then
+	if [ ! -d $WORKDIR/$ARCH/build ]; then
 		cd $WORKDIR
 		if [ ! -f $WORKDIR/buildroot-$BUILDROOT_VERSION.tar ]; then
 			wget http://www.buildroot.net/downloads/buildroot-$BUILDROOT_VERSION.tar.bz2
 			bzip2 -d buildroot-2011.05.tar.bz2
 		fi
 		tar -xf buildroot-$BUILDROOT_VERSION.tar
-		mv buildroot-2011.05 build_$ARCH
-		touch .prepared
+		mv buildroot-2011.05 $ARCH/build
+		touch $ARCH/build/.prepared
 	fi
 	
 	echo "Staging workdir..."
@@ -90,10 +90,10 @@ stage_pulsar ()
 	cp -r $BASE/frontend $WORKDIR/
 	cp -r $BASE/installer $WORKDIR/
 	cp -r $BASE/backend/startupscripts $WORKDIR/
-	mkdir $WORKDIR/core && mkdir $WORKDIR/sdk
+	mkdir $WORKDIR/$ARCH/core && mkdir $WORKDIR/$ARCH/sdk
 	# necessary to track release changes in corepackages (only for internal development) 
-	[ ! -d $WORKDIR/corepackages ] && cp -r $BASE/backend/corepackages $WORKDIR/
-	cp $BASE/backend/configs/buildroot_$ARCH.config $WORKDIR/build_$ARCH/.config
+	[ ! -d $PACKAGE_DIR ] && cp -r $BASE/backend/corepackages $WORKDIR/$ARCH/
+	cp $BASE/backend/configs/buildroot_$ARCH.config $WORKDIR/$ARCH/build/.config
 }
 
 make_pulsar ()
@@ -102,52 +102,56 @@ make_pulsar ()
 	cd $WORKDIR
 	
 	# copy external changes to buildroot framework
-	cp $BASE/backend/configs/e2fsprogs.mk $WORKDIR/build_$ARCH/package/e2fsprogs/
+	cp $BASE/backend/configs/e2fsprogs.mk $WORKDIR/$ARCH/build/package/e2fsprogs/
 	
 	# copy all 3rd party packages to buildroot
-	cp -r $BASE/backend/packages/* $WORKDIR/build_$ARCH/package
-	if [ `grep -c PulsarOS $WORKDIR/build_$ARCH/package/Config.in` = 0 ]; then
-		echo 'menu "Additional PulsarOS packages"' >> $WORKDIR/build_$ARCH/package/Config.in
-		echo 'source "package/monit/Config.in"' >> $WORKDIR/build_$ARCH/package/Config.in
-		echo 'source "package/extlinux/Config.in"' >> $WORKDIR/build_$ARCH/package/Config.in
-		echo 'source "package/pacman/Config.in"' >> $WORKDIR/build_$ARCH/package/Config.in
-		echo 'source "package/volmgr/Config.in"' >> $WORKDIR/build_$ARCH/package/Config.in
-		echo 'source "package/swraid/Config.in"' >> $WORKDIR/build_$ARCH/package/Config.in
-		echo 'source "package/sdkbinutils/Config.in"' >> $WORKDIR/build_$ARCH/package/Config.in
-		echo 'source "package/sdkgcc/Config.in"' >> $WORKDIR/build_$ARCH/package/Config.in
-		echo 'source "package/sdkgmp/Config.in"' >> $WORKDIR/build_$ARCH/package/Config.in
-		echo 'source "package/sdkmpc/Config.in"' >> $WORKDIR/build_$ARCH/package/Config.in
-		echo 'source "package/sdkmpfr/Config.in"' >> $WORKDIR/build_$ARCH/package/Config.in
-		echo 'source "package/sdkuclibc/Config.in"' >> $WORKDIR/build_$ARCH/package/Config.in
-		echo 'source "package/sshpass/Config.in"' >> $WORKDIR/build_$ARCH/package/Config.in
-		echo 'source "package/msmtp/Config.in"' >> $WORKDIR/build_$ARCH/package/Config.in
-		echo 'source "package/gnutls/Config.in"' >> $WORKDIR/build_$ARCH/package/Config.in
-		echo 'source "package/wget_ssl/Config.in"' >> $WORKDIR/build_$ARCH/package/Config.in
-		echo 'endmenu' >> $WORKDIR/build_$ARCH/package/Config.in
+	cp -r $BASE/backend/packages/* $WORKDIR/$ARCH/build/package
+	if [ `grep -c PulsarOS $WORKDIR/$ARCH/build/package/Config.in` = 0 ]; then
+		echo 'menu "Additional PulsarOS packages"' >> $WORKDIR/$ARCH/build/package/Config.in
+		echo 'source "package/monit/Config.in"' >> $WORKDIR/$ARCH/build/package/Config.in
+		echo 'source "package/extlinux/Config.in"' >> $WORKDIR/$ARCH/build/package/Config.in
+		echo 'source "package/pacman/Config.in"' >> $WORKDIR/$ARCH/build/package/Config.in
+		echo 'source "package/volmgr/Config.in"' >> $WORKDIR/$ARCH/build/package/Config.in
+		echo 'source "package/swraid/Config.in"' >> $WORKDIR/$ARCH/build/package/Config.in
+		echo 'source "package/sdkbinutils/Config.in"' >> $WORKDIR/$ARCH/build/package/Config.in
+		echo 'source "package/sdkgcc/Config.in"' >> $WORKDIR/$ARCH/build/package/Config.in
+		echo 'source "package/sdkgmp/Config.in"' >> $WORKDIR/$ARCH/build/package/Config.in
+		echo 'source "package/sdkmpc/Config.in"' >> $WORKDIR/$ARCH/build/package/Config.in
+		echo 'source "package/sdkmpfr/Config.in"' >> $WORKDIR/$ARCH/build/package/Config.in
+		echo 'source "package/sdkuclibc/Config.in"' >> $WORKDIR/$ARCH/build/package/Config.in
+		echo 'source "package/sshpass/Config.in"' >> $WORKDIR/$ARCH/build/package/Config.in
+		echo 'source "package/msmtp/Config.in"' >> $WORKDIR/$ARCH/build/package/Config.in
+		echo 'source "package/gnutls/Config.in"' >> $WORKDIR/$ARCH/build/package/Config.in
+		echo 'source "package/wget_ssl/Config.in"' >> $WORKDIR/$ARCH/build/package/Config.in
+		echo 'endmenu' >> $WORKDIR/$ARCH/build/package/Config.in
 	fi
 	
 	# build the backend
-	cd $WORKDIR/build_$ARCH && make
+	cd $WORKDIR/$ARCH/build && make
 	
 	# copy kernel and basesystem to $BOOT_HOME
-	cp $WORKDIR/build_$ARCH/output/images/bzImage $BOOT_HOME/
-	cp $WORKDIR/build_$ARCH/output/images/rootfs.ext2.bz2 $BOOT_HOME/initrd.bz2
+	if [ $ARCH == "arm" ]; then
+		cp $WORKDIR/$ARCH/build/output/images/zImage $BOOT_HOME/
+	else
+		cp $WORKDIR/$ARCH/build/output/images/bzImage $BOOT_HOME/
+	fi
+	cp $WORKDIR/$ARCH/build/output/images/rootfs.ext2.bz2 $BOOT_HOME/initrd.bz2
 	
 		
 	# move base packages to BOOT
-	cd $WORKDIR/core/ && wget -r -l 1 -nd --accept pkg.tar.gz http://repo.pulsaros.com/core_dev
+	cd $WORKDIR/$ARCH/core/ && wget -r -l 1 -nd --accept pkg.tar.gz http://repo.pulsaros.com/$ARCH/core_dev
 	# we use the new build versions
-	rm $WORKDIR/core/kernel* && rm $WORKDIR/core/basesystem* rm $WORKDIR/core/frontend*
+	rm $WORKDIR/$ARCH/core/kernel* && rm $WORKDIR/$ARCH/core/basesystem* rm $WORKDIR/$ARCH/core/frontend*
 	rm robots.txt
-	cp $WORKDIR/core/* $WORKDIR/boot_$ARCH/core
+	cp $WORKDIR/$ARCH/core/* $WORKDIR/$ARCH/boot/core
 	
 	# copy needed gpg headers (for development reasons) to gcc package
-	cp $WORKDIR/build_$ARCH/output/build/libgcrypt-*/src/gcrypt.h $PACKAGE_DIR/gcc/local/include/
-	cp $WORKDIR/build_$ARCH/output/build/libgcrypt-*/src/gcrypt-module.h $PACKAGE_DIR/gcc/local/include/
-	cp $WORKDIR/build_$ARCH/output/build/libgpg-error-*/src/gpg-error.h $PACKAGE_DIR/gcc/local/include/
+	cp $WORKDIR/$ARCH/build/output/build/libgcrypt-*/src/gcrypt.h $PACKAGE_DIR/gcc/local/include/
+	cp $WORKDIR/$ARCH/build/output/build/libgcrypt-*/src/gcrypt-module.h $PACKAGE_DIR/gcc/local/include/
+	cp $WORKDIR/$ARCH/build/output/build/libgpg-error-*/src/gpg-error.h $PACKAGE_DIR/gcc/local/include/
 	
 	# copy needed gnutls headers (for development reasons) to gcc package
-	cp -r $WORKDIR/build_$ARCH/output/build/gnutls-*/lib/includes/gnutls $PACKAGE_DIR/gcc/local/include/
+	cp -r $WORKDIR/$ARCH/build/output/build/gnutls-*/lib/includes/gnutls $PACKAGE_DIR/gcc/local/include/
 	rm $PACKAGE_DIR/gcc/local/include/gnutls/gnutls.h.in
 	
 	# build gcc package
@@ -156,28 +160,28 @@ make_pulsar ()
 	cat $PACKAGE_DIR/gcc/PKGBUILD| sed "s|pkgrel=$PKGVERSION|pkgrel=$NEWPKGVERSION|g" > PKGBUILD_TMP
 	mv PKGBUILD_TMP $PACKAGE_DIR/gcc/PKGBUILD
 	cd $PACKAGE_DIR/gcc && makepkg -f --skipinteg
-	cp $PACKAGE_DIR/gcc/gcc-$VERSION-* $WORKDIR/sdk
+	cp $PACKAGE_DIR/gcc/gcc-$VERSION-* $WORKDIR/$ARCH/sdk
 	
 	# build kernel package
-	cp $WORKDIR/build_$ARCH/output/images/bzImage $PACKAGE_DIR/kernel/
+	cp $WORKDIR/$ARCH/build/output/images/bzImage $PACKAGE_DIR/kernel/
 	PKGVERSION=`cat $PACKAGE_DIR/kernel/PKGBUILD|grep pkgrel|awk -F= '{print $2}'`
 	NEWPKGVERSION=$(($PKGVERSION+1))
 	cat $PACKAGE_DIR/kernel/PKGBUILD| sed "s|pkgrel=$PKGVERSION|pkgrel=$NEWPKGVERSION|g" > PKGBUILD_TMP
 	mv PKGBUILD_TMP $PACKAGE_DIR/kernel/PKGBUILD
 	cd $PACKAGE_DIR/kernel && makepkg -f --skipinteg
-	cp $PACKAGE_DIR/kernel/kernel-$VERSION-* $WORKDIR/boot_$ARCH/core/
-	cp $PACKAGE_DIR/kernel/kernel-$VERSION-* $WORKDIR/core/
+	cp $PACKAGE_DIR/kernel/kernel-$VERSION-* $WORKDIR/$ARCH/boot/core/
+	cp $PACKAGE_DIR/kernel/kernel-$VERSION-* $WORKDIR/$ARCH/core/
 	
 	# build basesystem package
-	cp $WORKDIR/build_$ARCH/output/images/rootfs.ext2.bz2 $PACKAGE_DIR/basesystem/initrd.bz2
+	cp $WORKDIR/$ARCH/build/output/images/rootfs.ext2.bz2 $PACKAGE_DIR/basesystem/initrd.bz2
 	cp -r $WORKDIR/pulsarroot $PACKAGE_DIR/basesystem/
 	PKGVERSION=`cat $PACKAGE_DIR/basesystem/PKGBUILD|grep pkgrel|awk -F= '{print $2}'`
 	NEWPKGVERSION=$(($PKGVERSION+1))
 	cat $PACKAGE_DIR/basesystem/PKGBUILD| sed "s|pkgrel=$PKGVERSION|pkgrel=$NEWPKGVERSION|g" > PKGBUILD_TMP
 	mv PKGBUILD_TMP $PACKAGE_DIR/basesystem/PKGBUILD
 	cd $PACKAGE_DIR/basesystem && sudo makepkg -f --skipinteg --asroot
-	cp $PACKAGE_DIR/basesystem/basesystem-$VERSION-* $WORKDIR/boot_$ARCH/core/
-	cp $PACKAGE_DIR/basesystem/basesystem-$VERSION-* $WORKDIR/core/
+	cp $PACKAGE_DIR/basesystem/basesystem-$VERSION-* $WORKDIR/$ARCH/boot/core/
+	cp $PACKAGE_DIR/basesystem/basesystem-$VERSION-* $WORKDIR/$ARCH/core/
 	
 	# cleanup package directories
 	sudo rm -r $PACKAGE_DIR/basesystem/initrd* $PACKAGE_DIR/basesystem/pkg $PACKAGE_DIR/basesystem/src $PACKAGE_DIR/basesystem/basesystem-$VERSION-*
@@ -193,8 +197,8 @@ build_frontend ()
 	cat $PACKAGE_DIR/frontend/PKGBUILD| sed "s|pkgrel=$PKGVERSION|pkgrel=$NEWPKGVERSION|g" > PKGBUILD_TMP
 	mv PKGBUILD_TMP $PACKAGE_DIR/frontend/PKGBUILD
 	cd $PACKAGE_DIR/frontend && sudo makepkg -f --skipinteg --asroot
-	cp $PACKAGE_DIR/frontend/frontend-$VERSION-* $WORKDIR/boot_$ARCH/core/
-	cp $PACKAGE_DIR/frontend/frontend-$VERSION-* $WORKDIR/core
+	cp $PACKAGE_DIR/frontend/frontend-$VERSION-* $WORKDIR/$ARCH/boot/core/
+	cp $PACKAGE_DIR/frontend/frontend-$VERSION-* $WORKDIR/$ARCH/core
 	sudo rm -r $PACKAGE_DIR/frontend/frontend $PACKAGE_DIR/frontend/pkg $PACKAGE_DIR/frontend/src $PACKAGE_DIR/frontend/frontend-$VERSION-*
 }
 
@@ -203,7 +207,7 @@ make_image ()
 	echo "Make image"
 	cp $BASE/gpl.txt $BASE/backend/configs/isolinux.bin $BASE/backend/configs/isolinux.cfg $BOOT_HOME
 	cd $WORKDIR
-	sudo genisoimage -r -J -pad -b boot/isolinux/isolinux.bin -c boot/isolinux/boot.cat -no-emul-boot -boot-info-table -boot-load-size 4 -o $WORKDIR/images/pulsaros_$ARCH.iso boot_$ARCH
+	sudo genisoimage -r -J -pad -b boot/isolinux/isolinux.bin -c boot/isolinux/boot.cat -no-emul-boot -boot-info-table -boot-load-size 4 -o $WORKDIR/images/pulsaros_$ARCH.iso $ARCH/boot
 }
 
 # Main script
